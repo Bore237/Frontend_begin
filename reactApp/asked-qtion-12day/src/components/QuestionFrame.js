8896f3fb586319f13210bc6716a7843ec2103835
Borel <goudjouborel@gmail.com>
2023-06-10 10:25:52 +0200

    End with the question react project

--- reactApp/asked-qtion-12day/src/components/QuestionFrame.js
+++ reactApp/asked-qtion-12day/src/components/QuestionFrame.js
@@ -1,27 +1,44 @@
 import '../style/QuestionFrame.css'
 import { Icon } from '@iconify/react';
+import { useState } from 'react';
 
-function displayResp(id){
-    const response = document.getElementById(`resp${id}`);
-    const element = document.querySelector(`[key="${id}"]`);
-    const frame = element.getAttribute('.frame');
-    response.classList.add('show');
-    frame.classList.add('show');
+function displayResp(id, isOpen){
+    const frame = document.getElementById(`frame${id}`);
+    const response = frame.querySelector('.reponse');
+    if(isOpen){ 
+        response.classList.add('show');
+        frame.classList.add('show');
+    }else{
+        response.classList.remove('show');
+        frame.classList.remove('show');
+    }
 }
 
-
 function QuestionFrame({reponse, question, id}){
+    const [isOpen, setIsOpen] = useState(false);
+    const [icone, setIcone] = useState("majesticons:chevron-down");
+
 
     return(
-        <div className='frame' key={id}>
+        <div className='frame' id={'frame'+id}>
             <div className='question'> 
                 <h3>{question}</h3>
             </div>
-            <div className='btn' onClick={() => displayResp(id)}>  
-                <Icon icon="majesticons:chevron-down" color="gray" width="30" />
-                {/* <Icon icon="carbon:close-filled" color="gray" width="30"/> */}
+            <div className='btn' 
+                onClick={() =>  {  
+                if(isOpen){
+                    displayResp(id, isOpen);
+                    setIcone("carbon:close-filled");
+                    setIsOpen(false);
+                }else{ 
+                    displayResp(id, isOpen);
+                    setIcone("majesticons:chevron-down");
+                    setIsOpen(true);
+                }}
+            }>
+                <Icon icon={icone} color="gray" width="30" />
             </div>
-            <div className='reponse' id={'resp'+id}> <p> {reponse} </p></div>
+            <div className='reponse'> <p> {reponse} </p></div>
         </div>         
     )
 }
