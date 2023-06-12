3c65042db5dc2bc72393e743cb1eef947c63c809
Borel <goudjouborel@gmail.com>
2023-06-12 17:20:50 +0200

    Ajout the overview on the cart

--- reactApp/nav-film-foot-14-15-17day/src/components/CartMovie.js
+++ reactApp/nav-film-foot-14-15-17day/src/components/CartMovie.js
@@ -3,8 +3,23 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 
 function CartMovie({id, title, img, note, overview}){    
 
+function showOverview(id, val){
+    const idCart = document.getElementById(`cart${id}`)
+    const overviewCart = idCart.querySelector('.Cart-overview');
+    //Ajout and remove overview
+    val ?  overviewCart.classList.add('show') :
+    overviewCart.classList.remove('show')
+}
+
     return(
-        <div className='Cart-containt' >
+        <div key={id+'fff'} id={'cart'+id} className='Cart-containt' 
+            onMouseMove={() =>{
+                showOverview(id, true);
+            }}
+            onMouseLeave={() =>{
+                showOverview(id, false);
+            }}
+        >
             <img src= {IMG_PATH + img} alt= {title} />
             <div className='Cart-info'>
                 <h3> {title}  </h3> 
--- reactApp/nav-film-foot-14-15-17day/src/components/CartMovieList.js
+++ reactApp/nav-film-foot-14-15-17day/src/components/CartMovieList.js
@@ -4,9 +4,7 @@ import "../style/CartMovieList.css";
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 
 function CartMovieList(){
-    const [dataMovie, setDataMovie] = useState([])
-    let count = 0;
-
+    const [dataMovie, setDataMovie] = useState([]);
 
     //use useEffet for fresh the element after create the coponenent
     useEffect(()=>{
@@ -17,7 +15,7 @@ function CartMovieList(){
     }, [])
 
     return(
-        <div className="Cart-list"> 
+        <div  className="Cart-list"> 
             {dataMovie.map(movie =>(
                 <CartMovie
                     id={movie.id}
