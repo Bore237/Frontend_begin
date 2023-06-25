d97a4f3fe58b559435be41f7d84c4edc012ccbad
Borel <goudjouborel@gmail.com>
2023-06-25 09:15:34 +0200

    ajout the jsx element that containt the progress bar and test imonial

--- reactApp/cont-placehoer-24days/src/components/Testimonial47.jsx
+++ reactApp/cont-placehoer-24days/src/components/Testimonial47.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { Icon } from '@iconify/react';
+import '../styles/Testimonial47.css'
 
 class Testimonial extends React.Component{
     constructor(props){
@@ -7,10 +8,14 @@ class Testimonial extends React.Component{
     }
 
     render(){
-        
+        const data = this.props.text
         return(
             <div className="containt"> 
-
+                <div className="grid-text">
+                    <Icon icon="bi:quote" color="white" width="30" height="30" rotate={2} />
+                    <p className="text"> {data} </p>
+                    <Icon icon="bi:quote" color="white" width="30" height="30" />
+                </div>
             </div>
         )
     }
