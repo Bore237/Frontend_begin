e277a00a9e883fd170f667a894a4606c7d93dbbe
Borel <goudjouborel@gmail.com>
2023-06-25 15:48:48 +0200

    Create un compoment that display the description of user.

--- reactApp/cont-placehoer-24days/src/components/Testimonial47.jsx
+++ reactApp/cont-placehoer-24days/src/components/Testimonial47.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import { Icon } from '@iconify/react';
 import '../styles/Testimonial47.css'
 
+
+function Userprofil({photo,name,position }){
+    return(
+    <div className="containt-profil">
+        <img src={photo} alt="profil photo" />
+        <h4 > {name} </h4>
+        <p>{position}</p>
+    </div>
+    )
+}
+
 class Testimonial extends React.Component{
     constructor(props){
         super(props);
@@ -10,14 +21,20 @@ class Testimonial extends React.Component{
     render(){
         const data = this.props.datas
         console.log("text datas", data)
+        const {text, name, photo, position} = data[0]
         return(
             <div className="containt"> 
                 <div className="grid-text">
                     <div className="progress-bar"> </div>
                     <Icon className= "icon-left" icon="bi:quote" color="white" width="30" height="30" rotate={2} />
-                    <p className="text"> {data[0].text} </p>
+                    <p className="text"> {text} </p>
                     <Icon className= "icon-right" icon="bi:quote" color="white" width="30" height="30" />
                 </div>
+                <Userprofil 
+                    photo={photo}
+                    position={position}
+                    name={name}
+                />
             </div>
         )
     }
